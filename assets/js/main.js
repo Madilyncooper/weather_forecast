7f5e99b0cfe732f6bb62b51de75c625b5e8dbf9e
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-03-27 15:08:17 -0700

    Added Dayjs for current day and began to add the additional days for each 5-day forecast card. Created 5-day forecast cards. Added in flexbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,36 @@
 openWeatherKey = 'f3e720e15d3544494b126bdef5a6745b';
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function updateTime() {
+    var todaysTime = dayjs();
+    document.querySelector('#currentDay').textContent = todaysTime.format('ddd, MM/DD/YYYY');
+
+    var plusOne = todaysTime.add(1, 'day');
+    document.querySelector('#plus-one').textContent = plusOne.format('ddd, MM/DD/YYYY')
+  }
+  
+  updateTime();
+
